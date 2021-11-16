62e736759ee8c43ae123953b206f0b908b3e4c3f
Brendan J. Kelly <brendank@upenn.edu>
2021-11-15 23:12:39 -0500

    Update pdf.

--- scripts/print_html_slides_to_pdf.R
+++ scripts/print_html_slides_to_pdf.R
@@ -10,4 +10,6 @@ set.seed(16)
 
 #' print slides to PDF
 pagedown::chrome_print(input = "measures_of_disease_in_epidemiology_2021_bjk.html",
-                       output = "~/Downloads/measures_of_disease_in_epidemiology_2021_bjk.pdf")
+                       output = "measures_of_disease_in_epidemiology.pdf",
+                       wait = 30,
+                       timeout = 180)
